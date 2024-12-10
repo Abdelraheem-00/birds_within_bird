84d8912a82c5cb65032b1130988c9e49f3995539
Abdelraheem-00 <therah7ememail@gmail.com>
2024-12-10 21:58:36 +0200

    update maze

--- main_maze.py
+++ main_maze.py
@@ -2,7 +2,7 @@ from maze import *
 
 class Food:
     def __init__(self):
-         self.img = pygame.image.load('resources\images\pig_failed.png').convert_alpha()
+        self.img = pygame.image.load('resources/images/pig_failed.png').convert_alpha()
         self.img = pygame.transform.scale(self.img, (TILE - 10, TILE - 10))
         self.rect = self.img.get_rect()
         self.set_pos()
@@ -62,14 +62,12 @@ game_surface = pygame.Surface(RES)
 surface = pygame.display.set_mode((WIDTH + 300, HEIGHT))
 clock = pygame.time.Clock()
 
-# images
-bg_game = pygame.image.load('resources/images/background2.jpg').convert()
+bg_game = pygame.image.load('resources/images/background1.jpg').convert()
 bg = pygame.image.load('maze_assets/21191 .jpg').convert()
 
-# get maze
+
 maze = generate_maze()
 
-# player settings
 player_speed = 5
 player_img = pygame.image.load('resources/images/red-bird.png').convert_alpha()
 player_img = pygame.transform.scale(player_img, (TILE - 2 * maze[0].thickness, TILE - 2 * maze[0].thickness))
@@ -79,19 +77,14 @@ directions = {'a': (-player_speed, 0), 'd': (player_speed, 0), 'w': (0, -player_
 keys = {'a': pygame.K_a, 'd': pygame.K_d, 'w': pygame.K_w, 's': pygame.K_s}
 direction = (0, 0)
 
-# food settings
 food_list = [Food() for i in range(3)]
 
-# collision list
 walls_collide_list = sum([cell.get_rects() for cell in maze], [])
-
-# timer, score, record
 pygame.time.set_timer(pygame.USEREVENT, 1000)
 time = 60
 score = 0
 record = get_record()
 
-# fonts
 font = pygame.font.SysFont('Impact', 150)
 text_font = pygame.font.SysFont('Impact', 80)
 
@@ -120,7 +113,6 @@ while True:
         if event.type == pygame.USEREVENT:
             time -= 1
 
-    # Controls and movement
     pressed_key = pygame.key.get_pressed()
     for key, key_value in keys.items():
         if pressed_key[key_value] and not is_collide(*directions[key]):
@@ -134,19 +126,15 @@ while True:
 
     [cell.draw(game_surface) for cell in maze]
 
-    # Gameplay logic
     if eat_food():
         FPS += 10
         score += 1
     is_game_over()
 
-    # Draw player
     game_surface.blit(player_img, player_rect)
 
-    # Draw food
     [food.draw() for food in food_list]
 
-    # Draw stats
     surface.blit(text_font.render('TIME', True, pygame.Color('white')), (WIDTH + 70, 30))
     surface.blit(font.render(f'{time}', True, pygame.Color('white')), (WIDTH + 70, 130))
 
@@ -156,6 +144,5 @@ while True:
     surface.blit(text_font.render('record:', True, pygame.Color('white')), (WIDTH + 30, 620))
     surface.blit(font.render(f'{record}', True, pygame.Color('white')), (WIDTH + 70, 700))
 
-    # Update display and limit frame rate
     pygame.display.flip()
     clock.tick(FPS)
